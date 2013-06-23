ea05de8642dc83c9db4d6e227c1ea7545c961191
Christelle Gaulupeau <cesagxoops@gmail.com>
2013-06-23 17:22:38 +0000

    Moving xbooking, xcgal and xdirectory non english files to XoopsLanguages folder.

--- xdirectory/language/nederlands/blocks.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define("_MB_XDIR_DISP","Toon maximaal ");
-define("_MB_XDIR_LINKS"," adressen.");
-define("_MB_XDIR_CHARS","Toon de eerste ");
-define("_MB_XDIR_LENGTH"," karakters van de title.");
-
-?>
